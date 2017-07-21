5a32d4171f408d82471029aec5349d9398a3a697
csy <csy>
2017-07-21 17:18:04 +0800

    imok

---
+++ study/imok/1.NetRequest/NetRequest/app/src/main/java/com/example/csy/netrequest/utils/Dbug.java
@@ -0,0 +1,37 @@
+package com.example.csy.netrequest.utils;
+
+import android.util.Log;
+
+public class Dbug {
+	private static boolean IS_DEBUG = true;
+	private final static String HeadStr = "CSY====LOG>>>";
+	public static void openOrCloseDebug(boolean isOpen) {
+		IS_DEBUG = isOpen;
+	}
+
+	public static void v(String tag, String msg) {
+		if(IS_DEBUG){
+			Log.v(tag, HeadStr+msg);
+		}
+	}
+	public static void d(String tag, String msg) {
+		if(IS_DEBUG){
+			Log.d(tag, HeadStr+msg);
+		}
+	}
+	public static void i(String tag, String msg) {
+		if(IS_DEBUG){
+			Log.i(tag, HeadStr+msg);
+		}
+	}
+	public static void w(String tag, String msg) {
+		if(IS_DEBUG){
+			Log.w(tag, HeadStr+msg);
+		}
+	}
+	public static void e(String tag, String msg) {
+		if(IS_DEBUG){
+			Log.e(tag, HeadStr+msg);
+		}
+	}
+}
