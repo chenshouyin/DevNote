f3c6a808c6d7199146e7690d4dbbf53b63abc528
陈守印 <786538586@qq.com>
2017-12-13 23:39:50 +0800

    解决action找不到问题

--- 后台开发篇/JAVAEE/Struct2msb/src/com/csy/struct2/HelloStruct2.java
+++ 后台开发篇/JAVAEE/Struct2msb/src/com/csy/struct2/HelloStruct2.java
@@ -1,13 +1,27 @@
 package com.csy.struct2;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HelloStruct2 extends ActionSupport{
+public class HelloStruct2 extends ActionSupport {
 
 	@Override
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("====HelloStruct2");
-		return "success";
+		return "execute";
 	}
 
+	
+	public String index()  {
+		// TODO Auto-generated method stub
+		System.out.println("====HelloStruct2");
+		return "SUCCESS";
+	}
+//	
+	
+	public String add()  {
+		// TODO Auto-generated method stub
+		System.out.println("====HelloStruct2");
+		return "ADD";
+	}
 }
