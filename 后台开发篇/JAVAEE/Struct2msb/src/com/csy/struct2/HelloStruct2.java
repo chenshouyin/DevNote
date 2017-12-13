a9b8900977284f12910d5b30b0836fc22b297327
csy <csy@csy>
2017-12-13 18:19:36 +0800

    Struct2

---
+++ 后台开发篇/JAVAEE/Struct2msb/src/com/csy/struct2/HelloStruct2.java
@@ -0,0 +1,13 @@
+package com.csy.struct2;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloStruct2 extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("====HelloStruct2");
+		return super.execute();
+	}
+
+}
