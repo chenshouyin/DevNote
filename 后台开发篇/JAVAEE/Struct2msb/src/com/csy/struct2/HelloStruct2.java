6649478af51fc452cb45752177bcc38a7eda2c61
csy <csy@csy>
2017-12-14 18:33:30 +0800

    解决action找不到问题

--- 后台开发篇/JAVAEE/Struct2msb/src/com/csy/struct2/HelloStruct2.java
+++ 后台开发篇/JAVAEE/Struct2msb/src/com/csy/struct2/HelloStruct2.java
@@ -7,21 +7,21 @@ public class HelloStruct2 extends ActionSupport {
 	@Override
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("====HelloStruct2");
-		return "execute";
+		System.out.println("====HelloStruct  Exception");
+		return "success";
 	}
 
 	
 	public String index()  {
 		// TODO Auto-generated method stub
-		System.out.println("====HelloStruct2");
+		System.out.println("====HelloStruct2 index");
 		return "SUCCESS";
 	}
+////	
 //	
-	
 	public String add()  {
 		// TODO Auto-generated method stub
-		System.out.println("====HelloStruct2");
+		System.out.println("====HelloStruct2 add");
 		return "ADD";
 	}
 }
