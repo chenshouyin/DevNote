864dc7c4536b516a679f802bbb4668949b3cf959
陈守印 <786538586@qq.com>
2018-07-04 23:38:10 +0800

    rectclerview

---
+++ 0csy/TestDevToolProject/library/src/main/java/com/csy/view/adapter/base/animation/BaseAnimation.java
@@ -0,0 +1,11 @@
+package com.csy.view.adapter.base.animation;
+
+import android.animation.Animator;
+import android.view.View;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface BaseAnimation {
+    Animator[] getAnimators(View view);
+}
---
+++ 0csy/TestDevToolProject/viewLib/src/main/java/com/csy/view/adapter/base/entity/MultiItemEntity.java
@@ -0,0 +1,8 @@
+package com.csy.view.adapter.base.entity;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface MultiItemEntity {
+    int getItemType();
+}
